d3b406703802308cf235b41785bd3a06d0e6bb53
anamattos <anajuliareis576@gmail.com>
2019-10-22 17:32:36 -0300

    Versão 1.0
    Aula 22/10/2019

---
+++ ProjetoAula2210/ProjetoAula2210/Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoAula2210.Models
+{
+    public partial class Produtos
+    {
+        public int ProdCodigo { get; set; }
+        public string ProdNome { get; set; }
+        public decimal ProdPreco { get; set; }
+        public int ProdQtde { get; set; }
+    }
+}
---
+++ ProjetoAula2210/ProjetoAula2210/Models/banco2210Context.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjetoAula2210.Models
+{
+    public partial class banco2210Context : DbContext
+    {
+        public banco2210Context()
+        {
+        }
+
+        public banco2210Context(DbContextOptions<banco2210Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produtos> Produtos { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produtos>(entity =>
+            {
+                entity.HasKey(e => e.ProdCodigo);
+
+                entity.Property(e => e.ProdCodigo).HasColumnName("Prod_Codigo");
+
+                entity.Property(e => e.ProdNome)
+                    .IsRequired()
+                    .HasColumnName("Prod_Nome")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ProdPreco)
+                    .HasColumnName("Prod_Preco")
+                    .HasColumnType("decimal(14, 2)");
+
+                entity.Property(e => e.ProdQtde).HasColumnName("Prod_Qtde");
+            });
+        }
+    }
+}
